fad1a4b9aeff5c3b7110c0621c4478c0e0fc32dc
MagicGnome <didaccolominas@gmail.com>
2021-01-13 19:58:35 +0100

    Commit DigitalSignature 1

--- src/Data/DigitalSignature.java
+++ src/Data/DigitalSignature.java
@@ -1,4 +1,27 @@
 package Data;
 
-public class DigitalSignature {
+final public class DigitalSignature {
+
+    private final byte[] doctorSignature;
+
+    public DigitalSignature(byte[] code) { this.doctorSignature = code; }
+    public DigitalSignature(){throw new IllegalArgumentException("falte un argument"); }
+    public byte[] getdoctorSignature() { return doctorSignature; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DigitalSignature dSignature = (DigitalSignature) o;
+        return doctorSignature.equals(dSignature.doctorSignature);
+    }
+    @Override
+    public int hashCode() { return doctorSignature.hashCode(); }
+
+    @Override
+    public String toString() {
+        return "HealthCardID{" + "personal code='" + doctorSignature + '\'' + '}';
 }
+
+}
+
